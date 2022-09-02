bcfa0c96241c6136bc822cba4496da0d6c866f04
Attribewx <98205105+Attribewx@users.noreply.github.com>
2022-09-02 12:03:02 -0700

    Add files via upload

---
+++ MovingLedgeContact.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingLedgeContact : MovingPlatform
+{
+    private bool hasContacted;
+    private Vector3 startPOS;
+    [SerializeField, Header("Platform Movement"), Space(10)]
+    private Vector3 targPOS;
+    private Vector3 speedTracked;
+    private Vector3 dir;
+    [SerializeField]
+    private float stopTime;
+    private bool goingLeft;
+    private bool cooling;
+    
+    new void Start()
+    {
+        base.Start();
+        startPOS = transform.position;
+        dir = (targPOS - startPOS).normalized;
+
+        if(dir.x < 0)
+        {
+            goingLeft = true;
+        }
+
+        speedTracked = speed;
+        speed = Vector3.zero;
+    }
+
+    new void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!cooling)
+        {
+            hasContacted = true;
+            speed = dir * speedTracked.x;
+            ChangeLightColors(changeLightColor);
+            ChangeMaterial(changeMaterial);
+
+        }
+        base.OnCollisionEnter2D(collision);
+
+
+    }
+
+    void Update()
+    {
+        if(hasContacted)
+        {
+            if(goingLeft && transform.position.x < targPOS.x && !cooling)
+            {
+                StartCoroutine(PlatformCooldown(stopTime));
+            }else if (!goingLeft && transform.position.x > targPOS.x && !cooling)
+            {
+                StartCoroutine(PlatformCooldown(stopTime));
+            }else if(cooling && goingLeft && transform.position.x > startPOS.x)
+            {
+                speed = Vector3.zero;
+                cooling = false;
+                ChangeMaterial(startMaterial);
+                ChangeLightColors(startLightColor);
+            }
+            else if (cooling && !goingLeft && transform.position.x < startPOS.x)
+            {
+                speed = Vector3.zero;
+                cooling = false;
+                ChangeMaterial(startMaterial);
+                ChangeLightColors(startLightColor);
+            }
+        }
+    }
+
+    IEnumerator PlatformCooldown(float time)
+    {
+        cooling = true;
+        speed = Vector3.zero;
+        yield return new WaitForSeconds(time);
+        speed = -dir * speedTracked.x;
+
+    }
+}
